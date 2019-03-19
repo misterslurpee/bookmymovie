64502e5ce2d7fa883e09ee2db4f998ac69ca6f59
M. Fairuzi Teguh <mfairuzit@gmail.com>
2019-03-19 12:28:33 +0700

    Restructuring the microservice folder

---
+++ purchase/src/main/java/com/adpro/purchase/web/HelloController.java
@@ -0,0 +1,13 @@
+package com.adpro.purchase.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String sayHello() {
+        return "Hello from purchase!";
+    }
+}

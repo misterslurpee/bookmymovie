89fefb40ff9d90cf095069bb53db30ad461a6dc3
Raihan Ramadistra <ramadistra@gmail.com>
2019-04-04 23:34:42 +0700

    Create TMDBClient

---
+++ movie/src/main/java/com/adpro/movie/tmdb/Page.java
@@ -0,0 +1,48 @@
+package com.adpro.movie.tmdb;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Page<T> {
+    private int page;
+    private int totalResults;
+    private int totalPages;
+    private List<T> results;
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getTotalResults() {
+        return totalResults;
+    }
+
+    @JsonProperty("total_results")
+    public void setTotalResults(int totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    @JsonProperty("total_pages")
+    public void setTotalPages(int totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public List<T> getResults() {
+        return results;
+    }
+
+    public void setResults(List<T> results) {
+        this.results = results;
+    }
+}
---
+++ movie/src/main/java/com/adpro/movie/tmdb/TMDBMovie.java
@@ -0,0 +1,39 @@
+package com.adpro.movie.tmdb;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.validation.constraints.NotNull;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TMDBMovie {
+    private String originalTitle;
+    private String overview;
+    private String posterPath;
+
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    @JsonProperty("original_title")
+    public void setOriginalTitle(String originalTitle) {
+        this.originalTitle = originalTitle;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    @JsonProperty("poster_path")
+    public void setPosterPath(@NotNull String posterPath) {
+        this.posterPath = posterPath;
+    }
+}

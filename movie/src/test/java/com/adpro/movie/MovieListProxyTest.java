a720f0be74c817b8a797a70009a323f7f4cf54c5
M. Fairuzi Teguh <mfairuzit@gmail.com>
2019-04-17 23:34:41 +0700

    Dome Movie List Proxy Test

---
+++ movie/src/test/java/com/adpro/movie/MovieListProxyTest.java
@@ -0,0 +1,31 @@
+package com.adpro.movie;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import java.time.LocalDate;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MovieListProxyTest {
+    @MockBean
+    MovieRepository movieRepository;
+
+    @Autowired
+    MovieListProxy movieListProxy;
+
+    @Test
+    public void givenUpdatedRecently_thenReturnFromMemory() {
+        movieListProxy.findMoviesByReleaseDateAfter(LocalDate.now());
+
+        movieListProxy.findMoviesByReleaseDateAfter(LocalDate.now());
+        verify(movieRepository, times(1))
+                .findMoviesByReleaseDateAfter(LocalDate.now());
+    }
+}

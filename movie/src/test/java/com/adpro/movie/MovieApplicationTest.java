a17d0c0fb0a46be35885087417f926f82e4b2e20
M. Fairuzi Teguh <mfairuzit@gmail.com>
2019-04-20 15:25:57 +0700

    Add test for movie session

--- movie/src/test/java/com/adpro/movie/MovieApplicationTest.java
+++ movie/src/test/java/com/adpro/movie/MovieApplicationTest.java
@@ -4,11 +4,12 @@ import static org.hamcrest.Matchers.is;
 import static org.mockito.BDDMockito.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.time.Duration;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -31,6 +32,9 @@ public class MovieApplicationTest {
 
 	@MockBean
 	private MovieListProxy movieListProxy;
+
+	@MockBean
+	private MovieSessionRepository movieSessionRepository;
 
 	@Test
 	public void contextLoads() {
@@ -55,5 +59,38 @@ public class MovieApplicationTest {
 				.andExpect(jsonPath("$[0].posterUrl", is(movie.getPosterUrl())))
 				.andExpect(jsonPath("$[0].releaseDate", is(movie.getReleaseDate().toString())))
 				.andExpect(jsonPath("$[0].duration", is("01:51:00")));
+	}
+
+	@Test
+	public void testRootRedirectToMovies() throws Exception {
+		this.mvc.perform(get("/"))
+				.andExpect(redirectedUrl("/movies"));
+	}
+
+	@Test
+	public void testGetMovieSessions() throws Exception {
+		Movie movie = Movie.builder()
+				.name("Fairuzi Adventures")
+				.description("Petualangan seorang Fairuzi")
+				.duration(Duration.ofMinutes(111))
+				.posterUrl("sdada")
+				.releaseDate(LocalDate.now())
+				.id(1L)
+				.build();
+
+		LocalDateTime dayTime = LocalDateTime.of(1999, 8, 10, 10, 0);
+		LocalDateTime nightTime = LocalDateTime.of(1999, 8, 10, 19, 0);
+		MovieSession daySession = new MovieSession(movie, dayTime);
+		MovieSession nightSession = new MovieSession(movie, nightTime);
+
+		given(movieSessionRepository.findMovieSessionsByMovieIdAndStartTimeAfter(any(), any()))
+				.willReturn(List.of(daySession, nightSession));
+
+		mvc.perform(get("/movie/1"))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$[0].startTime",
+						is(dayTime.format(DateTimeFormatter.ISO_DATE_TIME))))
+				.andExpect(jsonPath("$[1].startTime",
+						is(nightTime.format(DateTimeFormatter.ISO_DATE_TIME))));
 	}
 }
